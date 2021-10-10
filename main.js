17ad3c6971025c9bdff304e3d3873080aebb9ec4
Jan Badenhorst <jan@trintel.co.za>
2021-10-10 11:30:23 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,45 @@
+'use strict';
+import process from 'node:process';
+
+/**
+ * @param logger
+ * @returns {Set<Function|Promise>}
+ */
+export default (logger = undefined) => {
+  const log = logger || console;
+  const killSet = new Set();
+
+  const gracefulShutdown = async () => {
+    log.warn('Received kill signal, shutting down gracefully.');
+    await Promise.allSettled([...killSet].map(f => Promise.resolve(f())));
+    log.info('Shutdown Complete');
+    process.exit(0);
+  };
+
+  // Register listeners for kill signals
+  // listen for TERM signal .e.g. kill
+  process.once('SIGTERM', gracefulShutdown);
+
+  // listen for INT signal e.g. Ctrl-C
+  process.once('SIGINT', gracefulShutdown);
+
+  // Listen for pm2 shutdown message.
+  process.on('message', msg => {
+    if (msg === 'shutdown') {
+      gracefulShutdown().then(e => e);
+    }
+  });
+
+  return killSet;
+
+  // /**
+  //  * @param {Function|Promise} f
+  //  * @param {string} msg
+  //  */
+  // return (f, msg = undefined) => {
+  //   killSet.add(() =>
+  //       Promise.resolve(f()).then(() => {if (msg) { log.info(msg) }})
+  //   )
+  // }
+
+}
